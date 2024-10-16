860d8ea1eaf3d48a532b446b4297358cec560bbb
giwoong <dev.choi0409@gmail.com>
2024-10-16 09:21:21 +0900

    five

--- src/World.java
+++ src/World.java
@@ -3,4 +3,8 @@ public class World {
     public static void run() {
         System.out.println("202014098");
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
 }
